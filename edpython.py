03a5bd3fb3aeb6be0dfab1d6ec18cd9925d09c7b
Ruan Batista Rodrigues <145821191+zzruanxx@users.noreply.github.com>
2025-02-19 03:03:41 +0000

    treino de python com estrura de dados - inversao sequencia

---
+++ edpython.py
@@ -0,0 +1,9 @@
+def inverter_sequencia(sequencia):
+    return sequencia[::-1]
+
+#exmplo
+
+sequencia_original = [1, 2, 3, 4, 5]
+sequencia_invertida = inverter_sequencia(sequencia_original)
+print("Sequencia original:", sequencia_original)
+print("Sequencia invertida:", sequencia_invertida)
