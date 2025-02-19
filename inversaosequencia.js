d38ac12519c93069e382b4958ee984daef65102d
Ruan Batista Rodrigues <145821191+zzruanxx@users.noreply.github.com>
2025-02-19 03:43:42 +0000

    git add inversaosequencia.js

--- inversaosequencia.js
+++ inversaosequencia.js
@@ -1,1 +1,9 @@
+function inverterSequencia(sequencia) {
+    return sequencia.slice().reverse();
+}
 
+
+let sequenciaOriginal = [1, 2, 3, 4, 5];
+let sequenciaInvertida = inverterSequencia(sequenciaOriginal);
+console.log("Sequência original:", sequenciaOriginal);
+console.log("Sequência invertida:", sequenciaInvertida);
